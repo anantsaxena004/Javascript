732eb04c8ba1a8348fedfe53068a61c809ad2a8f
anantsaxena004 <saxenaanant004@gmail.com>
2024-07-26 12:35:19 +0000

    obj2 config done

---
+++ basics/objects2.js
@@ -0,0 +1,10 @@
+const tinderuser = new Object()
+tinderuser.id = "123abc"
+tinderuser.name = "Anant"
+tinderuser.isLoggedIn = false
+
+// console.log(tinderuser)
+
+const regularUser = {
+    email: "some@gmail.com"
+}
